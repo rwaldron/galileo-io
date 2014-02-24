6706dede66eed86f64d653ac3fe44a10e9044d55
Rick Waldron <waldron.rick@gmail.com>
2014-02-24 18:09:48 -0500

    Remove unnecessary call to pinMode
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- eg/blink.js
+++ eg/blink.js
@@ -3,7 +3,6 @@ var board = new Galileo();
 
 board.on("ready", function() {
   var byte = 0;
-  this.pinMode(9, this.MODES.OUTPUT);
 
   setInterval(function() {
     board.digitalWrite(9, (byte ^= 1));
